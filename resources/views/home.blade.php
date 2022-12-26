8385e15542902a193d186df7a9119e1d3b67d1d3
Farahzulfahamid14 <zulfafara1@gmail.com>
2022-12-26 23:01:49 +0700

    update docker dll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+</head>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 Route::resource('mahasiswa', MahasiswaController::class);
 
 /*
@@ -31,3 +32,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('mahasiswa/nilai/{Nim}', [MahasiwaController::class, 'nilai'])->name('nilai');
 
 Route::get('cetak_pdf/{Nim}',[MahasiwaController::class, 'cetak_pdf']);
+
+Route::get('/mig', function () {
+    // Call and Artisan command from within your application.
+    Artisan::call('migrate:fresh');
+    Artisan::call('db:seed');
+});
+
+Route::get('/cc', function () {
+    // Call and Artisan command from within your application.
+    Artisan::call('config:clear');
+});
